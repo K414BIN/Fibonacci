e41ae0ee8270b77ac4fca490af6b17120054eb4a
Alexander Kalabin <74727055+K414BIN@users.noreply.github.com>
2020-12-23 09:00:52 +0300

    Create matrix mul

--- Program.cs
+++ Program.cs
@@ -1,16 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
-namespace Lesson4_4
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Я допишу ее позднее
-        }
-    }
-}
+namespace Lesson4
+{     
+      class Program {
+       
+        static void Main()
+        {    
+            const  int iMatr=2;
+            int[,] c= new int[iMatr,iMatr] {
+                                            {1,1},
+                                            {1,0},
+                                                  };    
+             int[,] x = multi(c,c);
+             int[,]  matr = multi(x,c);
+             for (int i=0;i<matr.GetLength(0);i++) {
+                           				       Console.WriteLine();
+				                               for (int j=0;j<matr.GetLength(1);j++) 
+                                                                                                         {
+								                                  Console.Write("{0} ",     matr[i,j]);
+                                                                                                         }
+                                                       }
+           Console.WriteLine();
+       
+           }
+          static int[,] multi(int[,] a,int[,] b)
+          {
+             if (a.GetLength(1) !=b.GetLength(0)) throw new Exception ("Матрицы нельзя перемножить!!!");
+             int[,] r = new int[a.GetLength(0),b.GetLength(1)];
+             for ( int i=0; i < a.GetLength(0); i++)
+              {
+                for (int j=0; j< b.GetLength(1);j++)
+                  {  
+                     for (int k=0;k< b.GetLength(0);k++)
+                       {
+                          r[i,j]=a[i,k]*b[k,j];
+                       }
+                  }
+                }
+             return r;           
+           }
+           }
+//+++++++++++++++++++++
+                    }
