bdddbabc902ed08bf836f41a4d29fc5eaca0eed2
Alexander Kalabin <k414bin@yandex.ru>
2020-12-25 21:40:38 +0300

    Yes it is Live!

--- Program.cs
+++ Program.cs
@@ -8,9 +8,22 @@ namespace Lesson4_4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // Я допишу ее позднее
+         
+            Console.WriteLine("Добро пожаловать в программу поиска числа Фибоначчи порядка n. ");
+            Console.WriteLine("Введите искомое n:");
+            int x = Convert.ToInt32(System.Console.ReadLine());
+            Console.WriteLine("n={0} => {1}", x, fib(x));
+            Console.ReadLine();
+        }
+
+        static long fib(long n)
+        {
+            if (n == 0 || n == 1)
+                return n;
+
+            return fib(n - 1) + fib(n - 2);
         }
     }
 }
